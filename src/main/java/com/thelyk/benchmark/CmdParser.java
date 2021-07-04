b0f5f07628da77feebc8e8ac8b3d64191b424339
liuyikang <liuyk0109@gmail.com>
2021-07-05 00:47:26 +0800

    make opt and long opt string static

--- src/main/java/com/thelyk/benchmark/CmdParser.java
+++ src/main/java/com/thelyk/benchmark/CmdParser.java
@@ -6,15 +6,27 @@ public class CmdParser {
 
     private static final Options OPTIONS = new Options();
 
+    private static final String HELP_OPT = "h";
+    private static final String HELP_LONG_OPT = "help";
+    private static final String QPS_OPT = "q";
+    private static final String QPS_LONG_OPT = "qps";
+    private static final String SECONDS_OPT = "s";
+    private static final String SECONDS_LONG_OPT = "seconds";
+    private static final String METHOD_OPT = "m";
+    private static final String METHOD_LONG_OPT = "method";
+    private static final String URL_OPT = "u";
+    private static final String URL_LONG_OPT = "url";
+
     static {
-        OPTIONS.addOption("help", "usage help");
-        OPTIONS.addOption(Option.builder("q").longOpt("qps").hasArg().type(Long.class)
+        OPTIONS.addOption(Option.builder(HELP_OPT).longOpt(HELP_LONG_OPT)
+                .desc("usage help").build());
+        OPTIONS.addOption(Option.builder(QPS_OPT).longOpt(QPS_LONG_OPT).hasArg().type(Long.class)
                 .desc("queries per second").build());
-        OPTIONS.addOption(Option.builder("s").longOpt("seconds").required().hasArg().type(Long.class)
+        OPTIONS.addOption(Option.builder(SECONDS_OPT).longOpt(SECONDS_LONG_OPT).required().hasArg().type(Long.class)
                 .desc("seconds to benchmark").build());
-        OPTIONS.addOption(Option.builder("m").longOpt("method").hasArg().type(String.class)
+        OPTIONS.addOption(Option.builder(METHOD_OPT).longOpt(METHOD_LONG_OPT).hasArg().type(String.class)
                 .desc("http request method, default GET").build());
-        OPTIONS.addOption(Option.builder("u").longOpt("url").required().hasArg().type(String.class)
+        OPTIONS.addOption(Option.builder(URL_OPT).longOpt(URL_LONG_OPT).required().hasArg().type(String.class)
                 .desc("http request url").build());
     }
 
@@ -32,14 +44,14 @@ public class CmdParser {
             formatter.printHelp("benchmark", OPTIONS);
             return null;
         }
-        if (commandLine.hasOption("q")) {
-            args.setQps(Long.parseLong(commandLine.getOptionValue("q")));
+        if (commandLine.hasOption(QPS_OPT)) {
+            args.setQps(Long.parseLong(commandLine.getOptionValue(QPS_OPT)));
         }
-        args.setTestSeconds(Long.parseLong(commandLine.getOptionValue("s")));
-        if (commandLine.hasOption("m")) {
-            httpRequest.setMethod(commandLine.getOptionValue("q"));
+        args.setTestSeconds(Long.parseLong(commandLine.getOptionValue(SECONDS_OPT)));
+        if (commandLine.hasOption(METHOD_OPT)) {
+            httpRequest.setMethod(commandLine.getOptionValue(METHOD_OPT));
         }
-        httpRequest.setUrl(commandLine.getOptionValue("u"));
+        httpRequest.setUrl(commandLine.getOptionValue(URL_OPT));
         return args;
     }
 }
